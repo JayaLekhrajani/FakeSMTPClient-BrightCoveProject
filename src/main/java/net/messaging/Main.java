1625f8411db0384b8b37fade04dabdb774c1e335
Jaya Lekhrajani <jayalekhrajani26@gmail.com>
2018-05-15 17:27:23 -0700

    Story 2 Completed

--- src/main/java/net/messaging/Main.java
+++ src/main/java/net/messaging/Main.java
@@ -18,6 +18,22 @@ public class Main {
 
     	String email = args[0];
     	String message = args[1];
+    	
+    	//Checking if the email contains "@". Not checking if it contains ".com" since it was not specified
+    	if(!email.contains("@"))
+    	{
+    		try {
+    			console.write("Invalid email address: "+email+"\n");
+    		}
+    		catch(IOException e)
+    		{
+    			e.printStackTrace();
+    		}
+    		
+    		return; //Returning to prevent further processing.
+    	}
+    	
+    	//Writing to Network since no error
     	try {
 			network.write("connect smtp\nTo: "+email+"\n\n"+message+"\n\ndisconnect\n");
 		} catch (IOException e) {
